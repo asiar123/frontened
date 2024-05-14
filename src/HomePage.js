174948a01172eec56f822973ed24ae208e4c1160
asiar123 <michael_2504@hotmail.com>
2024-05-14 15:27:47 -0500

    16 commit

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,48 +1,61 @@
 import React, { useEffect } from 'react';
-import MapComponent from './components/MapComponent';
-import socket from './Socket';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faUsers, faComment } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faUsers, faTaxi } from '@fortawesome/free-solid-svg-icons'; // Suponiendo que añades el icono de un taxi
+import MapComponent from './components/MapComponent';
+import socket from './Socket';
 
 const HomePage = () => {
   const id_usuario = localStorage.getItem('id_usuario');
 
   useEffect(() => {
-    console.log('Socket connected in HomePage:', socket.connected);
+    console.log('Socket conectado en HomePage:', socket.connected);
   }, []);
 
-  // Styles
-  const iconsContainerStyle = {
-    position: 'absolute',
-    top: '20px',
-    right: '20px',
-    display: 'flex',
-    alignItems: 'center',
-    zIndex: 1000 // Ensure icons are above the map
-  };
-
-  const iconLinkStyle = {
-    marginLeft: '10px', // Space between icons
-    color: 'black', // Icon color
-    fontSize: '24px' // Icon size
-  };
-
   return (
-    <div>
-      <h1>Home Page</h1>
-      <div style={iconsContainerStyle}>
-        <Link to="/user" style={iconLinkStyle}>
-          <FontAwesomeIcon icon={faUser} />
-        </Link>
-        <Link to="/users-list" style={iconLinkStyle}>
-          <FontAwesomeIcon icon={faUsers} />
-        </Link>
-        <Link to="/chat" style={iconLinkStyle}>
-          <FontAwesomeIcon icon={faComment} />
-        </Link>
+    <div className="container-fluid" style={{ height: '100vh' }}>
+      <div className="row align-items-center" style={{ padding: '20px 0' }}>
+        <div className="col text-center">
+          <h1>Radio-taxis</h1>
+        </div>
+        <div className="col-auto" style={{ position: 'absolute', right: '20px', top: '20px' }}>
+          <Link to="/user" className="btn btn-primary">
+            <FontAwesomeIcon icon={faUser} size="lg" />
+          </Link>
+          <Link to="/users-list" className="btn btn-secondary ml-2">
+            <FontAwesomeIcon icon={faUsers} size="lg" />
+          </Link>
+          <Link to="/taxi-request" className="btn btn-success ml-2">
+            <FontAwesomeIcon icon={faTaxi} size="lg" />
+          </Link>
+        </div>
       </div>
-      <MapComponent id_usuario={id_usuario} />
+      <div className="row">
+        <div className="col-md-8" style={{ height: '85vh' }}>
+          <MapComponent id_usuario={id_usuario} />
+        </div>
+        <div className="col-md-4">
+          <form>
+            <div className="form-group">
+              <label htmlFor="originAddress">Dirección origen</label>
+              <input type="text" className="form-control" id="originAddress" placeholder="Ingresa la dirección de origen" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="destinationAddress">Dirección destino</label>
+              <input type="text" className="form-control" id="destinationAddress" placeholder="Ingresa la dirección de destino" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="price">Precio</label>
+              <input type="number" className="form-control" id="price" placeholder="Precio de la carrera" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="rate">Tarifa normal</label>
+              <input type="text" className="form-control" id="rate" placeholder="Tarifa aplicada" />
+            </div>
+            <button type="submit" className="btn btn-primary">Solicitar Taxi</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
